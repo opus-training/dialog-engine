1d33f7ff61f778b44cb57656ce1a7c7f471305df
Kai Yen <kai@purplesquirrel.io>
2020-04-18 13:31:40 -0400

    Allow mixed case language selection

--- stopcovid/drills/localize.py
+++ stopcovid/drills/localize.py
@@ -7,7 +7,7 @@ SUPPORTED_LANGUAGES = {"en", "es", "fr", "pt", "zh"}
 
 
 def localize(message: str, lang: Optional[str], **kwargs) -> str:
-    lang = lang or "en"
+    lang = lang.lower() or "en"
     if lang not in SUPPORTED_LANGUAGES:
         lang = "en"
     template = Template(message)
