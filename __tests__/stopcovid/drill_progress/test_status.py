7c8b9e4393ed34a95bf30b5303bf7e3e674c05e5
Kai Yen <kai@purplesquirrel.io>
2020-04-20 16:09:23 -0400

    Autocontinue drills

--- __tests__/stopcovid/drill_progress/test_status.py
+++ __tests__/stopcovid/drill_progress/test_status.py
@@ -1,22 +1,40 @@
 import logging
 import unittest
+from unittest.mock import MagicMock
+from uuid import uuid4
 
 from stopcovid.dialog.models.events import (
     DrillStarted,
     UserValidated,
     NextDrillRequested,
     DialogEventBatch,
+    DrillCompleted,
 )
 from stopcovid.dialog.registration import CodeValidationPayload
-from stopcovid.dialog.models.state import UserProfile
+from stopcovid.dialog.models.state import UserProfile, DialogState
 from stopcovid.drills.drills import get_drill
-from stopcovid.drill_progress.status import initiates_first_drill, initiates_subsequent_drill
+from stopcovid.drill_progress.status import (
+    initiates_first_drill,
+    initiates_subsequent_drill,
+)
 
 
 class TestStatus(unittest.TestCase):
     def setUp(self) -> None:
         logging.disable(logging.CRITICAL)
-        self.drill = get_drill("01-sample-drill")
+        self.phone_number = "123456789"
+        self.first_drill = get_drill("01-sample-drill")
+        self.intake_drill = get_drill("00-intake")
+        self.repo = MagicMock()
+        self.repo_1 = MagicMock()
+        self.dialog_state = DialogState(
+            current_drill=self.intake_drill, phone_number=self.phone_number, seq="0"
+        )
+        self.repo.fetch_dialog_state = MagicMock(return_value=self.dialog_state)
+        self.dialog_state_1 = DialogState(
+            current_drill=self.first_drill, phone_number=self.phone_number, seq="0"
+        )
+        self.repo_1.fetch_dialog_state = MagicMock(return_value=self.dialog_state_1)
 
     def test_initiates_first_drill(self):
         batch1 = DialogEventBatch(
@@ -31,8 +49,8 @@ class TestStatus(unittest.TestCase):
                 DrillStarted(
                     phone_number="123456789",
                     user_profile=UserProfile(True),
-                    drill=self.drill,
-                    first_prompt=self.drill.prompts[0],
+                    drill=self.first_drill,
+                    first_prompt=self.first_drill.prompts[0],
                 ),
             ],
         )
@@ -43,8 +61,8 @@ class TestStatus(unittest.TestCase):
                 DrillStarted(
                     phone_number="987654321",
                     user_profile=UserProfile(True),
-                    drill=self.drill,
-                    first_prompt=self.drill.prompts[0],
+                    drill=self.first_drill,
+                    first_prompt=self.first_drill.prompts[0],
                 )
             ],
         )
@@ -64,8 +82,8 @@ class TestStatus(unittest.TestCase):
                 DrillStarted(
                     phone_number="123456789",
                     user_profile=UserProfile(True),
-                    drill=self.drill,
-                    first_prompt=self.drill.prompts[0],
+                    drill=self.first_drill,
+                    first_prompt=self.first_drill.prompts[0],
                 ),
             ],
         )
@@ -76,10 +94,49 @@ class TestStatus(unittest.TestCase):
                 DrillStarted(
                     phone_number="987654321",
                     user_profile=UserProfile(True),
-                    drill=self.drill,
-                    first_prompt=self.drill.prompts[0],
+                    drill=self.first_drill,
+                    first_prompt=self.first_drill.prompts[0],
                 )
             ],
         )
-        self.assertTrue(initiates_subsequent_drill(batch1))
-        self.assertFalse(initiates_subsequent_drill(batch2))
+        self.assertTrue(initiates_subsequent_drill(batch1, self.repo))
+        self.assertFalse(initiates_subsequent_drill(batch2, self.repo))
+
+    def test_autocontinue_next_drill(self):
+        batch1 = DialogEventBatch(
+            phone_number="123456789",
+            seq="0",
+            events=[
+                DrillCompleted(
+                    drill_instance_id=uuid4(),
+                    phone_number="123456789",
+                    user_profile=UserProfile(True),
+                )
+            ],
+        )
+        batch2 = DialogEventBatch(
+            phone_number="123456789",
+            seq="0",
+            events=[
+                DrillCompleted(
+                    drill_instance_id=uuid4(),
+                    phone_number="123456789",
+                    user_profile=UserProfile(True),
+                )
+            ],
+        )
+        batch3 = DialogEventBatch(
+            phone_number="987654321",
+            seq="1",
+            events=[
+                DrillStarted(
+                    phone_number="987654321",
+                    user_profile=UserProfile(True),
+                    drill=self.intake_drill,
+                    first_prompt=self.intake_drill.prompts[0],
+                )
+            ],
+        )
+        self.assertTrue(initiates_subsequent_drill(batch1, self.repo))
+        self.assertFalse(initiates_subsequent_drill(batch2, self.repo_1))
+        self.assertFalse(initiates_subsequent_drill(batch3, self.repo))
--- stopcovid/drill_progress/status.py
+++ stopcovid/drill_progress/status.py
@@ -2,7 +2,13 @@ from typing import List
 
 from .initiation import DrillInitiator
 from .drill_progress import DrillProgressRepository
-from ..dialog.models.events import UserValidated, NextDrillRequested, DialogEventBatch
+from ..dialog.persistence import DialogRepository, DynamoDBDialogRepository
+from ..dialog.models.events import (
+    UserValidated,
+    NextDrillRequested,
+    DialogEventBatch,
+    DrillCompleted,
+)
 
 
 def handle_dialog_event_batches(batches: List[DialogEventBatch]):
@@ -14,9 +20,10 @@ def handle_dialog_event_batches(batches: List[DialogEventBatch]):
             initiator.trigger_first_drill(batch.phone_number, str(batch.batch_id))
 
     user_repo = DrillProgressRepository()
+    state_repo = DynamoDBDialogRepository()
     for batch in batches:
         user_repo.update_user(batch)
-        if initiates_subsequent_drill(batch):
+        if initiates_subsequent_drill(batch, state_repo):
             initiator.trigger_next_drill_for_user(batch.phone_number, str(batch.batch_id))
 
 
@@ -24,5 +31,12 @@ def initiates_first_drill(batch: DialogEventBatch):
     return any(event for event in batch.events if isinstance(event, UserValidated))
 
 
-def initiates_subsequent_drill(batch: DialogEventBatch):
-    return any(event for event in batch.events if isinstance(event, NextDrillRequested))
+def initiates_subsequent_drill(batch: DialogEventBatch, repo: DialogRepository):
+    for event in batch.events:
+        if isinstance(event, NextDrillRequested):
+            return True
+        elif isinstance(event, DrillCompleted):
+            dialog_state = repo.fetch_dialog_state(batch.phone_number)
+            current_drill = dialog_state.current_drill
+            if current_drill.auto_continue:
+                return True
