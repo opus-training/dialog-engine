1d826a44c934b40b4fc5424c4f32565446de4ab5
Per-Andre Stromhaug <perandrestromhaug@gmail.com>
2020-07-20 14:58:18 -0400

    add back reminder_triggered field

--- stopcovid/dialog/models/state.py
+++ stopcovid/dialog/models/state.py
@@ -83,6 +83,7 @@ class PromptStateSchema(Schema):
     slug = fields.Str(required=True)
     start_time = fields.DateTime(required=True)
     failures = fields.Int(allow_none=True)
+    reminder_triggered = fields.Boolean(allow_none=True, required=False)
 
     @post_load
     def make_prompt_state(self, data, **kwargs):
@@ -94,6 +95,7 @@ class PromptState:
     slug: str
     start_time: datetime.datetime
     last_response_time: Optional[datetime.datetime] = None
+    reminder_triggered: bool = False
     failures: int = 0
 
 
