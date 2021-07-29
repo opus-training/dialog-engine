3af4de973e3a84a91c710ebdd0730a5655d62be6
Taylor Burgess <tayloraburgess@gmail.com>
2021-07-29 14:39:40 -0500

    Remove test check

--- stopcovid/sms/send_sms.py
+++ stopcovid/sms/send_sms.py
@@ -11,7 +11,6 @@ from stopcovid.sms.types import SMSBatch, OutboundPayload
 from . import publish
 from ..utils.idempotency import IdempotencyChecker
 from ..utils.phones import is_fake_phone_number
-from ..utils.logging import _is_running_unit_tests
 
 DELAY_SECONDS_BETWEEN_MESSAGES = 3
 DELAY_SECONDS_AFTER_MEDIA = 10
@@ -44,7 +43,7 @@ def _publish_send(twilio_response: twilio.TwilioResponse, media_url: Optional[st
 
 
 def _send_batch(batch: SMSBatch) -> Optional[List[twilio.TwilioResponse]]:
-    if os.environ["STAGE"] == "local" and not _is_running_unit_tests():
+    if os.environ["STAGE"] == "local":
         logging.info(f"Local environment; skipping Twilio send: {batch}")
         return None
     if is_fake_phone_number(batch.phone_number):
